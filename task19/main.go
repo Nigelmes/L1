d2a804869a19109a4c878e69733516ce406dfd16
Nigelms <almazchic@gmail.com>
2023-06-29 16:13:11 +0300

    task19

--- task19/main.go
+++ task19/main.go
@@ -7,7 +7,7 @@ import (
 
 func reversefunc(arr string) string {
 	lenght := utf8.RuneCountInString(arr) // получаем количество символов в строке,
-	// len() - возвращает кол-во байт в строке, поэтому на не подходит
+	// len() - возвращает кол-во байт в строке, поэтому нам не подходит
 	res := []rune(arr) // создаем срез рун из строки
 	for i := 0; i < lenght/2; i++ {
 		res[i], res[lenght-1-i] = res[lenght-1-i], res[i] // в цикле переворачиваем элементы
