8380ec3104ed9a0fb744fc617017097c0c9b19bd
Nigelms <almazchic@gmail.com>
2023-07-03 13:03:03 +0300

    task14

--- task14/main.go
+++ task14/main.go
@@ -2,22 +2,23 @@ package main
 
 import "fmt"
 
-func printtype(arr ...interface{}) {
-	for _, elem := range arr { // итерируемся по переданным элементам
-		switch elem.(type) { // ключевым словом (type) получаем тип элемента
-		case int:
-			fmt.Printf("[%v] - int\n", elem)
-		case float64:
-			fmt.Printf("[%v] - float\n", elem) //принтуем тип
-		case string:
-			fmt.Printf("[%v] - string\n", elem)
-		case bool:
-			fmt.Printf("[%v] - bool\n", elem)
-		}
+func printtype(elem interface{}) { // функция для печати типа интерфейса
+	switch elem.(type) { // ключевым словом (type) получаем тип элемента
+	case int:
+		fmt.Printf("[%v] - int\n", elem)
+	case float64:
+		fmt.Printf("[%v] - float\n", elem) //принтуем тип
+	case string:
+		fmt.Printf("[%v] - string\n", elem)
+	case bool:
+		fmt.Printf("[%v] - bool\n", elem)
 	}
 }
 
 func main() {
 	arr := []interface{}{777, "Golang", 3.14, true}
-	printtype(arr...)
+	for _, elem := range arr { // итерируемся по слайсу интерфейса и передаем значения в функцию для определенния типа
+		printtype(elem)
+	}
+
 }
