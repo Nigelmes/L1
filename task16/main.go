ec760ce37e1161b84951b859d518dde4f75bd508
Nigelms <almazchic@gmail.com>
2023-07-03 15:18:57 +0300

    task16

--- task16/main.go
+++ task16/main.go
@@ -6,17 +6,19 @@ func QuickSort(arr []int) []int {
 	if len(arr) < 2 { // базовый случай для выхода из рекурсии
 		return arr
 	}
-	left, right := 0, len(arr)-1 // индексы границ массива
-	pivot := (left + right) / 2  //опорный индекс
-	arr[pivot], arr[right] = arr[right], arr[pivot]
+	left, right := 0, len(arr)-1                    // индексы границ массива
+	pivot := (left + right) / 2                     //опорный индекс
+	arr[pivot], arr[right] = arr[right], arr[pivot] // меняем местами значение опорного элемента с элементом
+	// расположенным в правой границе
 
 	for i := range arr {
 		if arr[i] < arr[right] {
-			arr[left], arr[i] = arr[i], arr[left]
+			arr[left], arr[i] = arr[i], arr[left] //в цикле помещает меньший элемент на левую сторону опорного элемента
 			left++
 		}
 	}
-	arr[left], arr[right] = arr[right], arr[left]
+	arr[left], arr[right] = arr[right], arr[left] //меняем местами опорный элемент,
+	// теперь он находится на своем окончательном месте
 
 	QuickSort(arr[:left])   //рекурсивный вызов левой части
 	QuickSort(arr[left+1:]) // рекурсивный вызов правой части
